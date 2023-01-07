6364a7959b8e652e962b7dfcea88d4807245d23b
Alexius <alexius@example.com>
2023-01-07 21:16:29 +0300

    auth in all pages

--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,20 @@
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login
 from django.shortcuts import render, redirect
 
+from authentication.forms import LoginForm
 
 def login_user(reqest):
-    return render(reqest, 'auth/login.html')
+    context = {'login_form': LoginForm()}
+    if reqest.method == 'POST':
+        login_form = LoginForm(reqest.POST)  # data from form
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(reqest, user)
+                return redirect('index')
+    return render(reqest, 'auth/login.html', context)
 
 def register(reqest):
     return render(reqest, 'auth/register.html')
